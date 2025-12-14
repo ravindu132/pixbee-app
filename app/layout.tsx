3d24d938ec673b5b5dfcabe8c3571b04f54abe47
ravinduhettiarachchi <ravinduhettiarachchi321@gmail.com>
2025-12-14 18:43:59 +0530

    Fixed missing RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,19 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+// This is the part that sets your App Icon
 export const metadata: Metadata = {
   title: "PixBee Agency",
   description: "Manage your creative empire",
@@ -10,3 +26,20 @@ export const metadata: Metadata = {
   themeColor: "#000000",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0", 
 };
+
+// This was the missing part!
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
